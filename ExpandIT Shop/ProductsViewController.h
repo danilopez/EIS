8a06acab74b9246a57041cccc9b20ce1a6279f15
Daniel López <tauron32@gmail.com>
2014-12-18 10:39:31 +0100

    Basic Products interface

---
+++ ExpandIT Shop/ProductsViewController.h
@@ -0,0 +1,15 @@
+//
+//  ProductsViewController.h
+//  ExpandIT Shop
+//
+//  Created by Daniel López on 18/12/14.
+//  Copyright (c) 2014 Daniel Lopez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProductsViewController : UIViewController<UITableViewDelegate, UITableViewDataSource>
+
+@property (nonatomic, strong) IBOutlet UITableView *tableView;
+
+@end
